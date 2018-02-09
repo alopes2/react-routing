d53447a2e8ca763fd03915924e723df1cff19b87
alopes2 <andre.lopes@exxonmobil.com>
2018-02-09 09:12:38 -0200

    loading full post through router params

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import './Blog.css';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-
+import FullPost from './FullPost/FullPost';
 class Blog extends Component {
 
 
@@ -26,8 +26,8 @@ class Blog extends Component {
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
-                                    'color': '#fa923f',
-                                    'text-decoration': 'underline'
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
                                 }}>Home</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
@@ -42,6 +42,7 @@ class Blog extends Component {
                 <Route path="/new-post" render={() => <NewPost />} /> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" exact component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
