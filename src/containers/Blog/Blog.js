5e768a837707b8ddacc577e0a4acbc6eef6d422f
alopes2 <andre.lopes@exxonmobil.com>
2018-02-09 09:18:21 -0200

    Using switch to load only one route at a time

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link, NavLink } from 'react-router-dom';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 
@@ -40,9 +40,11 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" exact render={() => <Posts />} />
                 <Route path="/new-post" render={() => <NewPost />} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
